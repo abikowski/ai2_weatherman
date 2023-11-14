deaa2919f93756f6e2af9b92f5a121170b7d4a24
Andrzej Bikowski <andi0597@gmail.com>
2023-11-14 15:01:05 +0100

    Created database entities and first location rows.

---
+++ src/Entity/Forecast.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ForecastRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ForecastRepository::class)]
+class Forecast
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 4, scale: 2)]
+    private ?string $temperature = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $humidity = null;
+
+    #[ORM\Column(length: 3, nullable: true)]
+    private ?string $wind_dir = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2, nullable: true)]
+    private ?string $wind_spd = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $time = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $timespan = null;
+
+    #[ORM\ManyToOne(inversedBy: 'forecasts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Location $location = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 4, scale: 3, nullable: true)]
+    private ?string $overcast = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTemperature(): ?string
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(string $temperature): static
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?int
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(?int $humidity): static
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+
+    public function getWindDir(): ?string
+    {
+        return $this->wind_dir;
+    }
+
+    public function setWindDir(?string $wind_dir): static
+    {
+        $this->wind_dir = $wind_dir;
+
+        return $this;
+    }
+
+    public function getWindSpd(): ?string
+    {
+        return $this->wind_spd;
+    }
+
+    public function setWindSpd(?string $wind_spd): static
+    {
+        $this->wind_spd = $wind_spd;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): static
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getTimespan(): ?\DateTimeInterface
+    {
+        return $this->timespan;
+    }
+
+    public function setTimespan(\DateTimeInterface $timespan): static
+    {
+        $this->timespan = $timespan;
+
+        return $this;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getOvercast(): ?string
+    {
+        return $this->overcast;
+    }
+
+    public function setOvercast(?string $overcast): static
+    {
+        $this->overcast = $overcast;
+
+        return $this;
+    }
+}
